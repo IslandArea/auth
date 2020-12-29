14483b8fc4bd95b22e42a4443c012c9a54f6b635
MadMatix <madmatix.pl@gmail.com>
2020-12-29 01:20:43 +0100

    v1.0.2

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
 const auth = require('./index.js');
 const authCode = new auth.authCode();
 
-console.log(authCode.createCode(), authCode.create2StepCode(), authCode.create3StepCode());
+console.log(authCode.createCode(), authCode.create2StepCode(), authCode.create3StepCode(01234567));
